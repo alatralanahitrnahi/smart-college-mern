461050a42b12e6f4a4ffed4f97adf31aa6e34520
Rohidas Lakade <rohidaslakade0707@gmail.com>
2026-01-12 13:14:30 +0530

    updated code added successfully

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000
-    });
-
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
