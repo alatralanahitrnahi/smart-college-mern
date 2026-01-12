b67f2f131035f72f60364a87bf52939f1bae7c30
Rohidas Raghu Lakade <126595636+Rohidas822@users.noreply.github.com>
2026-01-12 12:18:03 +0530

    Delete smart-college-mern directory

--- smart-college-mern/frontend/src/components/Error.jsx
+++
@@ -1,7 +0,0 @@
-export default function Error({ message }) {
-  return (
-    <div className="alert alert-danger text-center">
-      {message || "Something went wrong"}
-    </div>
-  );
-}
--- smart-college-mern/frontend/src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="text-center py-5">
-      <div className="spinner-border text-primary" />
-    </div>
-  );
-}
